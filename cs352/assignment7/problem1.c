89c69a60f136fe8e2f423ce63c63661d1595436d
Josh Davis <josh@joshldavis.com>
2013-03-11 13:47:07 -0500

    Add code for problem 1

--- cs352/assignment7/problem1.c
+++ cs352/assignment7/problem1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 int main(int argc, const char *argv[]) {
@@ -6,10 +7,37 @@ int main(int argc, const char *argv[]) {
     return 0;
 }
 
-void producer() {
+/* Monitor Header */
+
+typedef struct Monitor {
+    int in;
+    int out;
+    void *pool[10];
+    pthread_mutex_t producerMutex;
+    pthread_mutex_t consumerMutex;
+} Monitor;
+
+Monitor *initMonitor();
+void produce(Monitor *m, void *x);
+void consume(Monitor *m, void *y);
+
+/* Monitor Implementation */
+
+Monitor *initMonitor() {
+    Monitor *result = (Monitor *) malloc(sizeof(Monitor));
+
+    result->in = 0;
+    result->out = 0;
+    pthread_mutex_init(&(result->producerMutex), NULL);
+    pthread_mutex_init(&(result->consumerMutex), NULL);
+
+    return result;
+}
+
+void produce(Monitor *m, void *x) {
 
 }
 
-void consumer() {
+void consume(Monitor *m, void *y) {
 
 }
