28f2acb82c3f5fe03b665c0579534df8e76a979d
Josh Davis <josh@joshldavis.com>
2013-04-13 18:26:53 -0500

    Add template for actual implementation

---
+++ cs352/project2/pipe_sem.c
@@ -0,0 +1,16 @@
+#include <pipe_sem.h>
+
+/* Initalize a semaphore and set its initial value */
+void pipe_sem_init(pipe_sem_t *sem, int value) {
+
+}
+
+/* To perform a wait operation on the semaphore */
+void pipe_sem_wait(pipe_sem_t *sem) {
+
+}
+
+/* To perform a signal operation on the semaphore */
+void pipe_sem_signal(pipe_sem_t *sem) {
+
+}
