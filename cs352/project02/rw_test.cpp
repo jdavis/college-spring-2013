2f006423d5550d1569f141582ba52e31b94b2762
Josh Davis <josh@joshldavis.com>
2013-04-20 17:16:52 -0500

    Add various changes

--- cs352/project02/rw_test.cpp
+++ cs352/project02/rw_test.cpp
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <semaphore.h>
 
 typedef enum {
     EMPTY,
@@ -14,49 +15,90 @@ typedef enum {
 typedef struct {
     int waitingR;
     int waitingW;
+    int nReaders;
     state_t state;
 } WritersPriority;
 
 WritersPriority systemInfo;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+sem_t okToWrite;
+sem_t okToRead;
 
 void *reader(void *a) {
     pthread_t tid = pthread_self();
-    pthread_mutex_t *mutex = (pthread_mutex_t *) a;
+    int v;
+    sem_getvalue(&okToRead, &v);
+    printf("VAlue = %d\n", v);
 
+    pthread_mutex_lock(&mutex);
     /* Give priority to the writers */
-    if (systemInfo.waitingW != 0);
-
+    if (systemInfo.waitingW != 0) {
+        systemInfo.waitingR += 1;
+        sem_wait(&okToRead);
+        systemInfo.waitingR -= 1;
     /* Cannot access system when writer accessing */
-    else if (systemInfo.state == WRITING);
+    } else if (systemInfo.state == WRITING) {
+        systemInfo.waitingR += 1;
+        sem_wait(&okToRead);
+        systemInfo.waitingR -= 1;
+    }
+
+    systemInfo.state = READING;
+    systemInfo.nReaders += 1;
+    pthread_mutex_unlock(&mutex);
 
     printf("Reader thread %li enters CS\n", (long) tid);
 
     /* Read the db */
-    sleep(1);
+    sleep(3);
+
+    pthread_mutex_lock(&mutex);
+
+    systemInfo.nReaders -= 1;
+    if (systemInfo.waitingW) {
+        sem_post(&okToWrite);
+    } else if (systemInfo.nReaders == 0){
+        systemInfo.state = EMPTY;
+    }
 
     printf("Reader thread %li is exiting CS\n", (long) tid);
+    pthread_mutex_unlock(&mutex);
 
     return 0;
 }
 
 void *writer(void *a) {
     pthread_t tid = pthread_self();
-    pthread_mutex_t *mutex = (pthread_mutex_t *) a;
+    int v;
+    sem_getvalue(&okToRead, &v);
+    printf("VAlue = %d\n", v);
 
+    pthread_mutex_lock(&mutex);
     /* Only one writer can access system */
-    if (systemInfo.state != EMPTY);
+    if (systemInfo.nReaders != 0 || systemInfo.state == WRITING) {
+        systemInfo.waitingW += 1;
+        printf("nReaders != 0! Waiting...\n");
+        sem_wait(&okToWrite);
+        printf("Done waiting...\n");
+        systemInfo.waitingW -= 1;
+    }
 
     printf("Writer thread %li enters CS\n", (long) tid);
+    pthread_mutex_unlock(&mutex);
 
     /* Modify the db */
-    sleep(1);
+    sleep(3);
 
+    pthread_mutex_lock(&mutex);
     /* Give priority to waiting writers */
-    if (systemInfo.waitingW);
-    else if (systemInfo.waitingR);
+    if (systemInfo.waitingW) {
+        sem_post(&okToWrite);
+    } else if (systemInfo.waitingR) {
+        sem_post(&okToRead);
+    }
 
     printf("Writer thread %li is exiting CS\n", (long) tid);
-
+    pthread_mutex_unlock(&mutex);
 
     return 0;
 }
@@ -69,29 +111,29 @@ int usage() {
 int main(int argc, const char *argv[]) {
     int nThreads, interval, i;
     int nReaders, nWriters;
-    pthread_mutex_t *mutexes, *mutex;
+
     pthread_t *threads, *thread;
 
     if (argc < 3) return usage();
 
     nThreads = atoi(argv[1]);
 
-    mutexes = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t) * nThreads);
     threads = (pthread_t *) malloc(sizeof(pthread_t) * nThreads);
+    sem_init(&okToRead, 0, 0);
+    sem_init(&okToWrite, 0, 0);
 
     if (argc != 2 + nThreads + 1) return usage();
     interval = atoi(argv[2 + nThreads]);
 
     /* Gather number of readers/writers */
     for(i = 0; i < nThreads; i++) {
-        mutex = &mutexes[i];
         thread = &threads[i];
 
         if (strcmp("0", argv[i + 2]) == 0) {
-            pthread_create(thread, NULL, reader, (void *) mutex);
+            pthread_create(thread, NULL, reader, NULL);
             nReaders += 1;
         } else if (strcmp("1", argv[i + 2]) == 0) {
-            pthread_create(thread, NULL, writer, (void *) mutex);
+            pthread_create(thread, NULL, writer, NULL);
             nWriters += 1;
         } else {
             fprintf(stderr, "Invalid type of thread given\n");
@@ -105,3 +147,4 @@ int main(int argc, const char *argv[]) {
 
     return 0;
 }
+
