5164c84bdc18c838c20b8f0b2d9d11477d8630f7
Josh Davis <josh@joshldavis.com>
2013-04-20 19:47:40 -0500

    Implement pipe_sem functions

--- cs352/project02/pipe_sem.c
+++ cs352/project02/pipe_sem.c
@@ -1,16 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "pipe_sem.h"
 
 /* Initalize a semaphore and set its initial value */
 void pipe_sem_init(pipe_sem_t *sem, int value) {
-    return;
+    pipe(sem->fd);
+    sem->value = value;
 }
 
 /* To perform a wait operation on the semaphore */
 void pipe_sem_wait(pipe_sem_t *sem) {
-    return;
+    char buff[5];
+    if (sem->value == 0) {
+        read(sem->fd[0], buff, 5);
+    }
 }
 
 /* To perform a signal operation on the semaphore */
 void pipe_sem_signal(pipe_sem_t *sem) {
-    return;
+    write(sem->fd[1], "ok", 5);
 }
