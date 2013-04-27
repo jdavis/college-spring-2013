bdbf840851cc8d124cd41db14080f62cbbc6a5d6
Josh Davis <josh@joshldavis.com>
2013-04-26 19:58:14 -0500

    Add more comments

--- cs352/project02/rw_test.cpp
+++ cs352/project02/rw_test.cpp
@@ -6,17 +6,15 @@
 #include <sys/types.h>
 #include <semaphore.h>
 
-
+/*
+ * Include our pipe_sem.h as C code.
+ * I don't know why but the spec said for this file to be .cpp
+ */
 extern "C" {
     #include "pipe_sem.h"
 }
 
-typedef enum {
-    EMPTY,
-    READING,
-    WRITING,
-} state_t;
-
+/* Info that represents the RW Problem */
 typedef struct {
     int waitingR;
     int waitingW;
@@ -24,9 +22,13 @@ typedef struct {
     int writing;
 } WritersPriority;
 
+/* System state info */
 WritersPriority systemInfo;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/* Sem to control when it is okay to write */
 pipe_sem_t okToWrite;
+
+/* Sem to control when it is okay to read */
 pipe_sem_t okToRead;
 
 void *reader(void *a) {
@@ -43,19 +45,25 @@ void *reader(void *a) {
         systemInfo.waitingR -= 1;
     }
 
+    /* Reader is now reading, increment number of readers */
     systemInfo.nReaders += 1;
+
+    /* Signal other readers if there are others waiting */
     if (systemInfo.waitingR > 0 && systemInfo.waitingW == 0) {
         pipe_sem_signal(&okToRead);
     }
 
-    printf("Reader thread %li enters CS.\n", *tid);
+    printf("Reader thread %d enters CS.\n", *tid);
 
     /* Read the db */
     sleep(3);
 
-    printf("Reader thread %li is exiting CS.\n", *tid);
+    printf("Reader thread %d is exiting CS.\n", *tid);
 
+    /* Reader is finishing reading */
     systemInfo.nReaders -= 1;
+
+    /* Signal to waiting writers, if they are waiting */
     if (systemInfo.waitingW > 0 && systemInfo.nReaders == 0) {
         pipe_sem_signal(&okToWrite);
     }
@@ -77,16 +85,20 @@ void *writer(void *a) {
         systemInfo.waitingW -= 1;
     }
 
+    /* Update state to say that we are writing */
     systemInfo.writing = 1;
-    printf("Writer thread %li enters CS.\n", *tid);
+
+    printf("Writer thread %d enters CS.\n", *tid);
 
     /* Modify the db */
     sleep(3);
 
-    printf("Writer thread %li is exiting CS.\n", *tid);
+    printf("Writer thread %d is exiting CS.\n", *tid);
 
+    /* Update state to say no writer is writing */
     systemInfo.writing = 0;
-    /* Give priority to waiting writers */
+
+    /* Give priority to waiting writers, if no writers, signal to readers */
     if (systemInfo.waitingW) {
         pipe_sem_signal(&okToWrite);
     } else if (systemInfo.waitingR){
@@ -108,24 +120,28 @@ int main(int argc, const char *argv[]) {
 
     pthread_t *threads, *thread;
 
+    /* Check arguments */
     if (argc < 3) return usage();
 
     nThreads = atoi(argv[1]);
 
+    if (argc != 2 + nThreads + 1) return usage();
+    interval = atoi(argv[2 + nThreads]);
+
+    /* Create our arrays */
     threads = (pthread_t *) malloc(sizeof(pthread_t) * nThreads);
     tids = (int *) malloc(sizeof(int) * nThreads);
 
+    /* Init our pipes */
     pipe_sem_init(&okToRead, 0);
     pipe_sem_init(&okToWrite, 0);
-
-    if (argc != 2 + nThreads + 1) return usage();
-    interval = atoi(argv[2 + nThreads]);
 
     /* Gather number of readers/writers */
     for(i = 0; i < nThreads; i++) {
         thread = &threads[i];
         tids[i] = i;
 
+        /* If 0, launch new reader, if 1, launch new writer */
         if (strcmp("0", argv[i + 2]) == 0) {
             pthread_create(thread, NULL, reader, &tids[i]);
             nReaders += 1;
@@ -140,6 +156,7 @@ int main(int argc, const char *argv[]) {
         sleep(interval);
     }
 
+    /* Wait for all the pthreads to exit and finish */
     pthread_exit(NULL);
 
     return 0;
