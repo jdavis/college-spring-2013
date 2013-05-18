ac0a8bce1eaafdc6ce099e26374a4092d33eda36
Josh Davis <josh@joshldavis.com>
2013-05-17 20:06:31 -0500

    Remove cs229 code

--- cs229/project2/EmptyProbe.cpp
+++
@@ -1,8 +0,0 @@
-#include "EmptyProbe.h"
-#include <iostream>
-
-// nothing to probe!
-list<ISimObject *>& EmptyProbe::probe() {
-	list<ISimObject *> l;
-	return *(new list<ISimObject *>);
-}
--- cs229/project2/EnergyPill.h
+++
@@ -1,8 +0,0 @@
-#include "Interfaces.h"
-#include "SimObject.h"
-
-class EnergyPill : public SimObject {
-	public:
-		int getType();
-		int setPropForValue(string &p, string &v);
-};
